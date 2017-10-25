74cf41da36dd861262d7afeea65b856eb547912d
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-25 12:03:22 +0100

    Test get bike passed, challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ require "bike.rb"
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   it "is expected to create a new bike" do
-    expect(DockingStation.new.release_bike).to eq(Bike.new)
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
   end
 
 end
