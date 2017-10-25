3ed029a35d214776151cd264ed23d8fae3f03b15
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-25 15:44:23 +0100

    user story 1 passed, challenge 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,15 @@ require "docking_station"
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
+
   it "is expected to create a new bike" do
     expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
   end
+
   it "expects the bike to be working" do
     expect(Bike.new.working?).to eq true
   end
-  it "allows bikes to be docked in stations" do
-    expect(DockingStation.new).to respond_to(:docking).with(1).argument
-  end
-  it "see what's in the docking station" do
-    expect(DockingStation.new.inventory).to eq(@bike_inventory)
-  end
+
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
 end
