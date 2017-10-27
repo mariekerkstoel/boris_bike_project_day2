1825bb51c4b1241a32c38be1c57f50b0af932937
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-27 12:13:18 +0100

    experiment with mocking

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,18 @@
 require "docking_station"
 
+
+
 describe DockingStation do
+
+  let(:bike) { double :bike}
+
+
+
   it { is_expected.to respond_to(:release_bike) }
 
   it "expects the bike to be working" do
-    expect(Bike.new.working?).to eq true
+    allow(bike).to receive(:working?).and_return(true)
+    expect(bike.working?).to eq true
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
@@ -12,13 +20,13 @@ describe DockingStation do
   it { is_expected.to respond_to(:bikes) }
 
   it "dock bike is expected to return bike" do
-    test_bike = Bike.new
+    test_bike = bike
     expect(DockingStation.new.dock(test_bike)).to eq([test_bike])
   end
 
   it "The bike method should return bike" do
     station = DockingStation.new
-    test_bike_two = Bike.new
+    test_bike_two = bike
     station.dock(test_bike_two)
     expect(station.bikes).to eq([test_bike_two])
   end
@@ -29,14 +37,14 @@ describe DockingStation do
 
   it "should return bike if bikes available" do
     station2 = DockingStation.new
-    bike = Bike.new
+    bike = bike
     station2.dock(bike)
     expect(station2.release_bike).to eq(bike)
   end
 
   it "should raise error if docking station full" do
-    20.times{subject.dock(Bike.new)}
-    expect{subject.dock(Bike.new)}.to raise_error
+    20.times{subject.dock(bike)}
+    expect{subject.dock(bike)}.to raise_error
   end
 
   it "should be able to hold 20 bikes" do
