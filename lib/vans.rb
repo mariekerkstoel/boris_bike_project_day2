78e08cb3726458e91cb7ce92c0a41e1d7f7013b2
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-10-27 14:30:52 +0100

    Creates a  Van class that can take a bike

---
+++ lib/vans.rb
@@ -0,0 +1,10 @@
+class Van
+  attr_accessor :bikes_in_van
+
+  def initialize
+    @bikes_in_van = []
+  end
+  def take(bike)
+    @bikes_in_van.push(bike)
+  end
+end
---
+++ spec/vans_spec.rb
@@ -0,0 +1,14 @@
+require 'vans'
+
+describe Van do
+  # let(:bike) { double(:bike) }
+
+  describe "#take" do
+    it "should take broken bikes from ds" do
+      bike = "Something"
+      expect(subject).to respond_to(:take)
+      subject.take(bike)
+      expect(subject.bikes_in_van).to eq [bike]
+    end
+  end
+end
